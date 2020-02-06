7fee9205302f66e2b978de716abef9c10aee3509
Prasad Basutkar <prasadb@ebi.ac.uk>
2020-02-06 10:21:20 +0000

    added pipeline

--- java/javaTestCases.sh
+++ java/javaTestCases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #COMMAND="java  -classpath java/lib/*:java/EBeye_JAXRS.jar uk.ac.ebi.webservices.jaxrs.EBeyeClient --getNumberOfResults hgnc human --endpoint http://www.ebi.ac.uk/ebisearch/ws/rest -debugLevel 1 | grep -q '15' ; exit $?"
-export CLASSPATH=java/lib/*:java/EBeye_JAXRS.jar
-COMMAND="java uk.ac.ebi.webservices.jaxrs.EBeyeClient --getNumberOfResults hgnc human --endpoint http://www.ebi.ac.uk/ebisearch/ws/rest -debugLevel 1 | grep -q '15' ; echo $?"
+#COMMAND="java  -classpath 'java/lib/*':'java/EBeye_JAXRS.jar' uk.ac.ebi.webservices.jaxrs.EBeyeClient --getNumberOfResults hgnc human --endpoint http://www.ebi.ac.uk/ebisearch/ws/rest -debugLevel 1 | grep -q '15' ; echo $?"
+COMMAND="java  -classpath 'java/lib/*':'java/EBeye_JAXRS.jar' --getNumberOfResults hgnc human --endpoint http://www.ebi.ac.uk/ebisearch/ws/rest -debugLevel 1 | grep -q '15' ; echo $?"
 echo $COMMAND
 $COMMAND
