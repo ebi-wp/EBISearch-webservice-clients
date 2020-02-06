3defdb0660a0d9825be3976931e973f437fc987b
Prasad Basutkar <prasadb@ebi.ac.uk>
2020-02-06 13:52:06 +0000

    added pipeline

--- java/javaTestCases.sh
+++ java/javaTestCases.sh
@@ -3,6 +3,6 @@
 #COMMAND="java  -classpath java/lib/*:java/EBeye_JAXRS.jar uk.ac.ebi.webservices.jaxrs.EBeyeClient --getNumberOfResults hgnc human --endpoint http://www.ebi.ac.uk/ebisearch/ws/rest -debugLevel 1 | grep -q '15' ; exit $?"
 scriptPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #COMMAND="java  -cp ${scriptPath}/lib/*:${scriptPath}/EBeye_JAXRS.jar uk.ac.ebi.webservices.jaxrs.EBeyeClient"
-COMMAND="java -Djava.ext.dirs=${scriptPath}/lib/ -jar ${scriptPath}/jar/EBeye_JAXRS.jar"
+COMMAND="java -Djava.ext.dirs=${scriptPath}/lib/; -jar ${scriptPath}/jar/EBeye_JAXRS.jar"
 echo $COMMAND
 $COMMAND
